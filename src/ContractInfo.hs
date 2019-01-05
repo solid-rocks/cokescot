00e9908aa3bf61a0f458ee85cf549340374f383a
Max Taldykin <jorpic@gmail.com>
2019-01-06 00:25:26 +0300

    Delegate json parsing to external tools
    It is much faster and easier to e.g.
        $ jq -r '.address + " " + .bytecode' contracts.json
    instead of parsing this in Haskell.

--- src/ContractInfo.hs
+++ src/ContractInfo.hs
@@ -2,32 +2,16 @@
 
 module ContractInfo
   ( ContractInfo(..)
-  , fromJSON, fromBinary, toBinary
+  , fromText
   ) where
 
-import           Control.Applicative ((<|>))
-import           Control.Monad
-import           Control.Exception
-import           Data.Maybe (fromMaybe)
-
-import           Data.Aeson ((.:))
-import qualified Data.Aeson as Aeson
-import qualified Data.Aeson.Types as Aeson
-
+import           Data.Char (ord)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Builder as B
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as L8
 
-import           Data.Text (Text)
-import qualified Data.Text as T
-import           Data.Text.Internal.Read (hexDigitToInt)
-
-import           Data.Binary
-import           Data.Binary.Get (getByteString, getInt32le)
-import           Data.Binary.Put (putByteString, putInt32le, runPut)
-
-import           System.IO (IOMode(WriteMode), withBinaryFile, Handle)
+import           System.IO (Handle)
 
 
 const_ADDRESS_LENGTH :: Int
@@ -44,67 +28,38 @@ data ContractInfo = ContractInfo
 
 
 instance Show ContractInfo where
-  show ContractInfo{..} = show $ B.toLazyByteString
+  show ContractInfo{..}
+    = L8.unpack $ B.toLazyByteString
     $ B.byteStringHex addr <> B.char8 ' ' <> B.byteStringHex code
 
 
-instance Binary ContractInfo where
-  get = do
-    addr' <- getByteString const_ADDRESS_LENGTH
-    len   <- getInt32le
-    code' <- getByteString $ fromIntegral len
-    return $ ContractInfo addr' code'
-
-  put ContractInfo{..} = do
-    putByteString addr
-    putInt32le $ fromIntegral $ B.length code
-    putByteString code
+fromText :: Handle -> IO [ContractInfo]
+fromText h = do
+  let parse = \case
+        -- TODO: check addr length
+        [addr, code] -> ContractInfo (hexToBS addr) (hexToBS code)
+        [addr] -> ContractInfo (hexToBS addr) B.empty
+        line -> error $ show line
+  map (parse . L8.words) . L8.lines <$> L8.hGetContents h
 
 
-data ParseError = ParseError L.ByteString String deriving Show
-instance Exception ParseError
-
-
-fromBinary :: FilePath -> IO [ContractInfo]
-fromBinary f = loop <$> L.readFile f
-  where
-    loop bytes = case decodeOrFail bytes of
-      Left ("", _consumed, _err) -> []
-      Left (rest, _consumed, err) -> throw $ ParseError (L.take 200 rest) err
-      Right (rest, _consumed, res) -> res : loop rest
-
-
-fromJSON :: Handle -> IO [ContractInfo]
-fromJSON h = do
-  let parser jsn = do
-        addr' <- hexToBS <$> jsn .: "address"
-        code' <- hexToBS <$> jsn .: "bytecode"
-        when (B.length addr' /= const_ADDRESS_LENGTH)
-          $ fail "invalid address length"
-        return $ ContractInfo addr' code'
-  let parseRow r = Aeson.eitherDecode r >>= Aeson.parseEither parser
-
-  map (\r -> either (throw . ParseError r) id $ parseRow r)
-    . L8.lines
-    <$> L8.hGetContents h
-
-
-toBinary :: FilePath -> [ContractInfo] -> IO ()
-toBinary file ci = withBinaryFile file WriteMode
-  $ \h -> forM_ ci $ L.hPut h . runPut . put
-
-
-hexToBS :: Text -> B.ByteString
+-- FIXME: may fail
+hexToBS :: L.ByteString -> B.ByteString
 hexToBS hex
-  = L.toStrict $ B.toLazyByteString
-  $ mconcat $ map readByte
-  $ case T.length hex' `mod` 2 of
-    1 -> T.snoc "0" (T.head hex') : T.chunksOf 2 (T.tail hex')
-    _ -> T.chunksOf 2 hex'
+  = L.toStrict $ B.toLazyByteString $ mconcat $ map (B.word8 . fromIntegral)
+  $ loop hex'
   where
-    hex' = fromMaybe hex
-      $   T.stripPrefix "0x" hex
-      <|> T.stripPrefix "0X" hex
-    readByte byte = B.word8
-      $ fromIntegral
-      $ hexDigitToInt (T.head byte) * 16 + hexDigitToInt (T.last byte)
+    evenify xs = if even $ length xs then xs else '0':xs
+    hex' = evenify $ case L8.unpack hex of
+      '0':'x':xs -> xs
+      '0':'X':xs -> xs
+      xs -> xs
+    loop (a:b:xs) = (hexDigit a) * 16 + hexDigit b : loop xs
+    loop _ = []
+
+hexDigit :: Char -> Int
+hexDigit c
+  | '0' <= c && c <= '9' = ord c - ord '0'
+  | 'a' <= c && c <= 'f' = ord c - ord 'a' + 10
+  | 'A' <= c && c <= 'F' = ord c - ord 'A' + 10
+  | otherwise            = error "Invalid hex digit"
--- src/Main.hs
+++ src/Main.hs
@@ -1,34 +1,21 @@
 module Main where
 
-import           Control.Applicative
-import           Control.Monad
 import           Options.Applicative
 import           System.IO (stdin)
 import qualified ContractInfo as CI
 
 
-commands :: Parser (IO ())
-commands = subparser
-  $ (let commandArgs = argument str
-          (metavar "FILE" <> help "Write binary output to FILE")
-         cmd f = CI.fromJSON stdin >>= CI.toBinary f
-    in command "json-to-binary"
-      $ info (cmd <$> commandArgs)
-      $ progDesc
-        "Convert BigQuery's dump into a compact binary representation.")
-  <>
-    (let commandArgs = argument str
-          (metavar "FILE" <> help "Read binary format from FILE")
-    in command "read-binary"
-      $ info ((CI.fromBinary >=> mapM_ print) <$> commandArgs)
-      $ progDesc
-        "Run this command to test binary format reader.")
+data Options = Options
 
+options :: Parser Options
+options = pure Options
 
 main :: IO ()
-main = join
-  $ customExecParser (prefs showHelpOnEmpty)
-  $ info (commands <**> helper)
-  $ fullDesc <> progDesc
-    "Set of tools to analyse all the contracts\
-    \ on the Ethereum blockchain."
+main = do
+  Options <- customExecParser (prefs showHelpOnEmpty)
+      $ info (options <**> helper)
+      $ fullDesc <> progDesc
+        "Set of tools to analyse all the contracts\
+        \ on the Ethereum blockchain."
+
+  CI.fromText stdin >>= mapM_ print
