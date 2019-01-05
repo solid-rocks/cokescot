0aa7285c9a440428023cb9dd0d206b5c1e07dc38
Max Taldykin <jorpic@gmail.com>
2019-01-06 01:25:54 +0300

    +10% perf by using unboxed vectors instead of BS

--- src/ContractInfo.hs
+++ src/ContractInfo.hs
@@ -6,10 +6,11 @@ module ContractInfo
   ) where
 
 import           Data.Char (ord)
+import           Data.Word (Word8)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Builder as B
-import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.Vector.Unboxed as U
 
 import           System.IO (Handle)
 
@@ -17,8 +18,8 @@ import           System.IO (Handle)
 const_ADDRESS_LENGTH :: Int
 const_ADDRESS_LENGTH = 20
 
-type Address = B.ByteString
-type Code = B.ByteString
+type Address = U.Vector Word8
+type Code = U.Vector Word8
 
 
 data ContractInfo = ContractInfo
@@ -30,32 +31,36 @@ data ContractInfo = ContractInfo
 instance Show ContractInfo where
   show ContractInfo{..}
     = L8.unpack $ B.toLazyByteString
-    $ B.byteStringHex addr <> B.char8 ' ' <> B.byteStringHex code
+    $ B.byteStringHex (B.pack $ U.toList addr)
+      <> B.char8 ' '
+      <> B.byteStringHex (B.pack $ U.toList code)
 
 
 fromText :: Handle -> IO [ContractInfo]
 fromText h = do
   let parse = \case
         -- TODO: check addr length
-        [addr, code] -> ContractInfo (hexToBS addr) (hexToBS code)
-        [addr] -> ContractInfo (hexToBS addr) B.empty
+        -- TODO: check if valid hex
+        [addr, code] -> ContractInfo (hexToVec addr) (hexToVec code)
+        [addr] -> ContractInfo (hexToVec addr) U.empty
         line -> error $ show line
   map (parse . L8.words) . L8.lines <$> L8.hGetContents h
 
 
--- FIXME: may fail
-hexToBS :: L.ByteString -> B.ByteString
-hexToBS hex
-  = L.toStrict $ B.toLazyByteString $ mconcat $ map (B.word8 . fromIntegral)
-  $ loop hex'
+hexToVec :: L8.ByteString -> U.Vector Word8
+hexToVec hex
+  = U.fromListN (fromIntegral $ len' `div` 2) $ loop hex'
   where
-    evenify xs = if even $ L.length hex then xs else '0':xs
-    hex' = evenify $ case L8.unpack hex of
-      '0':'x':xs -> xs
-      '0':'X':xs -> xs
-      xs -> xs
-    loop (a:b:xs) = (hexDigit a) * 16 + hexDigit b : loop xs
+    evenify res@(len, xs) = if even len then res else (len + 1, '0' : xs)
+    (len', hex') = evenify $ case (L8.length hex, L8.unpack hex) of
+      (len, '0':'x':xs) -> (len - 2, xs)
+      (len, '0':'X':xs) -> (len - 2, xs)
+      res -> res
+    loop (a:b:xs)
+      = let x = (hexDigit a) * 16 + hexDigit b
+        in fromIntegral x : loop xs
     loop _ = []
+
 
 hexDigit :: Char -> Int
 hexDigit c
